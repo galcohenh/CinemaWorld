6e18584250134effa316c003eac757c96c54b91e
Gal Cohen <galcohenh@gmail.com>
2023-02-07 23:03:56 +0200

    [IMPROVE] remove order box script

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,4 +1,22 @@
-console.log("hre");
+const selectdate = document.querySelector("#selectdate");
+const today = new Date();
+
+for (let i = 0; i <= 4; i++) {
+  const option = document.createElement("option");
+  option.value = i;
+  option.textContent = new Date(today.getTime() + (i * 24 * 60 * 60 * 1000)).toDateString();
+  selectdate.appendChild(option);
+}
+
+
+const hours_options=[{text:'12:00'},{text:'13:00'},{text:'14:30'},{text:'16:00'},{text:'18:15'},{text:'19:00'},{text:'22:00'}]
+const selecthour = document.querySelector("#selecthour");
+for (let i = 0; i <hours_options.length; i++) {
+    const option = document.createElement("option");
+    option.value = i;
+    option.textContent = hours_options[i].text;
+    selecthour.appendChild(option);
+  }
 const posterContainer = document.querySelector('.poster-container');
 const posterIndex = Math.floor(Math.random() * 4) + 1;
 var poster = document.createElement("img");
