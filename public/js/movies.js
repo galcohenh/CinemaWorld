c084cad6d190a6293180e6c9aeab68a21bb58355
danielmargolin <margolin23@gmail.com>
2023-03-03 13:47:13 +0200

    Implemented redirect to screen page

--- public/js/movies.js
+++ public/js/movies.js
@@ -1,15 +1,6 @@
 const selectdate = document.querySelector("#selectdate");
 const today = new Date();
 
-for (let i = 0; i <= 4; i++) {
-  const option = document.createElement("option");
-  option.value = i;
-  option.textContent = new Date(
-    today.getTime() + i * 24 * 60 * 60 * 1000
-  ).toDateString();
-  selectdate.appendChild(option);
-}
-
 const hours_options = [
   { text: "12:00" },
   { text: "13:00" },
@@ -19,21 +10,33 @@ const hours_options = [
   { text: "19:00" },
   { text: "22:00" },
 ];
-const selecthour = document.querySelector("#selecthour");
-for (let i = 0; i < hours_options.length; i++) {
-  const option = document.createElement("option");
-  option.value = i;
-  option.textContent = hours_options[i].text;
-  selecthour.appendChild(option);
-}
-const posterContainer = document.querySelector(".poster-container");
-const posterIndex = Math.floor(Math.random() * 7) + 1;
-var poster = document.createElement("img");
-poster.src = `../assets/posters/poster${posterIndex}.png`;
-poster.className = "poster";
-posterContainer.appendChild(poster);
+
+// const posterContainer = document.querySelector(".poster-container");
+// const posterIndex = Math.floor(Math.random() * 7) + 1;
+// var poster = document.createElement("img");
+// poster.src = `../assets/posters/poster${posterIndex}.png`;
+// poster.className = "poster";
+// posterContainer.appendChild(poster);
 
 //--------------------------------
+function redirectToScreenPage(movieId, time) {
+  const queryString =
+    "?id=" + encodeURIComponent(movieId) + "&time=" + encodeURIComponent(time);
+
+  window.location.href = "http://localhost:8080/screen" + queryString;
+}
+
+$(document).ready(function () {
+  $(".order-tickets-form").submit(function (event) {
+    event.preventDefault();
+
+    var movieId = $("#selectmovie").val();
+    var time = $("#selecthour").val();
+
+    redirectToScreenPage(movieId, time);
+  });
+});
+
 function renderDayOptions(screens) {
   Object.keys(screens).forEach((screenDay) => {
     const optionElement = document.createElement("option");
@@ -41,6 +44,7 @@ function renderDayOptions(screens) {
     optionElement.textContent = screenDay;
     $("#selectdate").append(optionElement);
   });
+  
   $("#selectdate").change((e) => {
     const selectedDay = e.target.value;
     renderHours(screens[selectedDay]);
@@ -48,10 +52,10 @@ function renderDayOptions(screens) {
 }
 
 function renderHours(hours) {
-  hours.forEach((hour) => {
+  hours.forEach((screenHour) => {
     const optionElement = document.createElement("option");
-    optionElement.value = hour;
-    optionElement.textContent = hour;
+    optionElement.value = screenHour.fullDate;
+    optionElement.textContent = screenHour.hour;
     $("#selecthour").append(optionElement);
   });
 }
@@ -70,7 +74,7 @@ function onSelectMovieSearch(e) {
         const screenHour = `${dateOfScreen.getHours()}:${dateOfScreen.getMinutes()}`;
         availableScreens[dateKey] = availableScreens[dateKey]
           ? [...availableScreens[dateKey], screenHour]
-          : [screenHour];
+          : [{ hour: screenHour, fullDate: screen.time }];
       });
       renderDayOptions(availableScreens);
     },
